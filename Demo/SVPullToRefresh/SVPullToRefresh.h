49b2d397c87084dff1d48e4ba31199f744b78cbb
jamesyang17 <jamesyang@17.media>
2016-08-30 12:28:16 +0800

    support carthage

---
+++ Demo/SVPullToRefresh/SVPullToRefresh.h
@@ -0,0 +1,21 @@
+//
+//  SVPullToRefresh.h
+//  SVPullToRefresh
+//
+//  Created by James17 on 8/30/16.
+//  Copyright © 2016 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SVPullToRefresh.
+FOUNDATION_EXPORT double SVPullToRefreshVersionNumber;
+
+//! Project version string for SVPullToRefresh.
+FOUNDATION_EXPORT const unsigned char SVPullToRefreshVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVPullToRefresh/PublicHeader.h>
+
+#import <SVPullToRefresh/UIScrollView+SVPullToRefresh.h>
+#import <SVPullToRefresh/UIScrollView+SVInfiniteScrolling.h>
+
